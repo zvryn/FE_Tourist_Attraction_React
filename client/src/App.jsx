70b8bef24021fc3c88e22afce2bd88d2b209e039
zvryn <sarinkanok@gmail.com>
2024-11-06 11:56:18 +0700

    style:adjust style for mobile device

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import TripLists from "./components/TripLists";
 function App() {
   return (
     <div className="App">
-      <div>
+      <div className="w-full flex flex-col justify-center items-center">
         <h1 className="text-6xl text-primary mb-20 text-center">เที่ยวไหนดี</h1>
         <TripLists />
       </div>
--- client/src/components/TripLists.jsx
+++ client/src/components/TripLists.jsx
@@ -30,47 +30,60 @@ const TripLists = () => {
   console.log(trips);
 
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center w-full">
       <input
         type="text"
         placeholder="หาที่เที่ยวแล้วไปกัน ..."
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        className="w-[600px] p-2  mb-4 text-center border-b-2 bg-transparent border-gray-600"
+        className="w-full p-2  mb-4 text-center border-b-2 bg-transparent border-gray-600"
       />
 
       {trips.map((trip) => (
-        <div key={trip.eid} className="mb-8 flex">
+        <div
+          key={trip.eid}
+          className="mb-8 flex flex-col bg-white p-5 rounded-lg gap-3 border-black border"
+        >
           <img
             src={trip.photos[0]}
             alt={trip.title}
-            className="w-1/4 rounded-lg"
+            className="w-full rounded-lg object-cover h-80"
           />
-          <div className="ml-4 w-3/4">
+          <div className=" w-full flex flex-col items-start text-left justify-start">
             <h2 className="text-xl font-bold text-gray-800">{trip.title}</h2>
-            <p className="text-gray-600 mt-2">{trip.description}</p>
-            <p className="text-blue-500 mt-2">
-              <a href={trip.url}>อ่านต่อ</a>
+            <p className="text-gray-600 mt-2 line-clamp-3">
+              {trip.description}
             </p>
-            <p className="text-gray-500 text-sm mt-4">หมวด: {trip.tags}</p>
+            <p className="text-blue-500 mt-2">
+              <a
+                href={trip.url}
+                className="underline underline-offset-4 hover:font-semibold"
+              >
+                อ่านต่อ
+              </a>
+            </p>
+            <div className="text-gray-500 text-sm mt-4 flex space-x-1">
+              <p>{`หมวด:`} </p>
+              <a href="#" className="underline ">{`${trip.tags}`}</a>
+            </div>
             <div className="flex space-x-2 mt-2">
               <img
                 key={1}
                 src={trip.photos[1]}
                 alt="photo1"
-                className="w-16 h-16 rounded-lg object-cover"
+                className="w-32 h-32 rounded-lg object-cover"
               />
               <img
                 key={2}
                 src={trip.photos[2]}
                 alt="photo2"
-                className="w-16 h-16 rounded-lg object-cover"
+                className="w-32 h-32 rounded-lg object-cover"
               />
               <img
                 key={3}
                 src={trip.photos[3]}
                 alt="photo3"
-                className="w-16 h-16 rounded-lg object-cover"
+                className="w-32 h-32 rounded-lg object-cover"
               />
             </div>
           </div>
